a1ccda32ba98747a295011ccd76ce92f830d529f
shaye <sm0548540902@gmail.com>
2026-02-10 15:19:03 +0200

    finish

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Button from './button'
+
+function App() {
+
+  
+  return (
+    <div>
+    <h1>My Profile Page</h1>
+    <Button />
+    </div>
+  )
+}
+
+export default App
---
+++ vite-project/src/button.jsx
@@ -0,0 +1,45 @@
+
+import './button.css'
+
+
+function Button() {
+    return (
+        <div id='allBUttons'>
+            <div id='div'>
+                <h3>nerya malhi</h3>
+                <img className='img' src="https://simplesteps.co/images/animals/donky.jpg" alt="" />
+                <p className='gray'>ticher of my class</p>
+                <button className='ticher'>
+                    Ticher
+                </button>
+            </div>
+            <div id='div'>
+                <h3>yossi koban</h3>
+                <img className='img' src="https://simplesteps.co/images/animals/donky.jpg" alt="" />
+                <p className='gray'>frend of my class</p>
+                <button className='frend'>
+                    frend
+                </button>
+            </div>
+            <div id='div'>
+                <h3>yair grinberg</h3>
+                <img className='img' src="https://simplesteps.co/images/animals/donky.jpg" alt="" />
+                <p className='gray'>my studi in my class</p>
+                <button className='studi'>
+                    studi
+                </button>
+            </div>
+                        <div id='div'>
+                <h3>idan sananes</h3>
+                <img className='img' src="https://simplesteps.co/images/animals/donky.jpg" alt="" />
+                <p className='gray'>my studi in my class</p>
+                <button className='studi'>
+                    studi
+                </button>
+            </div>
+            <h2>© 2025 My App</h2>
+        </div>
+    )
+}
+
+export default Button
